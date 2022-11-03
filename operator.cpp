ddd2a565ba40c24c3a2e87cfba00ccd726bfe85d
恬恬 <109257015+rh198913@users.noreply.github.com>
2022-11-03 11:22:30 +0800

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+/* 运算符：用于执行代码的运算
+类型
+算术：用于处理四则运算
+赋值：用于将表达式的值赋值给变量 
+逻辑：根据表达式的值返回真值或假值
+比较：表达式比较，返回真值或假值
+ */
+int main()
+{
+    //算术运算
+    /* int a = 10;
+    int a1 = ++a * 10;
+    int b = 2;
+    int b1 = b++ * 10;
+   
+    cout << a+b<<endl;
+    cout << a-b<<endl;
+    cout << a*b<<endl;
+    cout << a/b<<endl;
+    cout << a%b<<endl;//取模求余数且两个小数不能取模运算
+    cout << a1 <<endl;
+    cout << b1 <<endl;
+     */
+    // 赋值运算
+    /* int a = 10;
+    a =100;
+    cout << "a = "<< a <<endl;
+    a += 2;
+    cout << "a = "<< a <<endl;
+    a -= 2;
+    cout << "a = "<< a <<endl;
+    a *= 2;
+    cout << "a = "<< a <<endl;
+    a /= 2;
+    cout << "a = "<< a <<endl; */
+    //比较运算符
+    /* int a = 10;
+    int b = 20;
+    cout << (a == b) << endl;//加括号有一个优先级的比较
+    cout << (a <= b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a != b) << endl; */
+    //逻辑运算符
+    /* int a = 10;
+    int b = 0;
+    cout <<(!a)<<endl;
+    cout <<(a&&b)<<endl;
+    cout <<(a||b)<<endl;
+    cout <<((a>0)||(b>0))<<endl;
+ */
+    system("pause");
+    return 0;
+
+}
