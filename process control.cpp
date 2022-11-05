0da9fb887006875ce0ec949324c9fa11854a6b8a
恬恬 <109257015+rh198913@users.noreply.github.com>
2022-11-05 10:11:22 +0800

    Add files via upload

---
+++ process control.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/* 流程控制结构
+顺序，选择，循环结构
+1选择结构 
+if语句，执行满足条件的语句，分为单行格式、多行格式，多条件格式
+嵌套if语句，用来达到更精确的条件判断 （第一层条件内再做个细化）*/
+int main()
+/* {
+    int p;
+    cout << "请输出所考的分数：" <<endl;
+    cin >> p;
+    if(p>=400){//if条件后面加分号就让条件分离了
+        cout<< "恭喜你考上985大学的研究生"<<endl;
+        if(p>420){
+            cout<<"重大稳了"<<endl;
+        }
+    }else if (p>390){
+        cout<<"复试很危险"<< endl;
+    }
+    else
+        cout<<"不许二战,赶紧学"<<endl;
+    system("pause");
+    return 0;
+} */
